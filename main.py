91b7f6d8dc2140c8522f71b4de7ac008215479a2
blzjn <blzjn65@gmail.com>
2023-07-20 12:02:30 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Importing tkinter and random modules
+from tkinter import *
+import random
+
+
+window = Tk()
+
+window.configure(bg="black")
+
+window.geometry("550x350")
+
+window.title("Dice Simulator")
+
+window.resizable(0, 0)
+
+def roll_dices():
+    dice_dots = ['\u2680', '\u2681',
+                 '\u2682', '\u2683',
+                 '\u2684', '\u2685']
+
+
+    label.configure(text=f'{random.choice(dice_dots)}')
+    label.pack()
+
+
+roll_button = Button(window, text="Roll!",width=10, height=2,font=15, bg="aqua",bd=2, command=roll_dices)
+
+roll_button.pack(padx=10, pady=15)
+
+label = Label(window, font=("times", 250),bg="black", fg="yellow")
+
+window.mainloop()
